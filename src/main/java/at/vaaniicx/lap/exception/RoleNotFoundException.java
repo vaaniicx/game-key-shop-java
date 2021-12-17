4ce3899ad5c1341fa52a1bc2723c0c4178c00f8f
Vanessa <vanessakausl@gmail.com>
2021-12-17 23:31:40 +0100

    Added custom Exceptions

---
+++ src/main/java/at/vaaniicx/lap/exception/RoleNotFoundException.java
@@ -0,0 +1,10 @@
+package at.vaaniicx.lap.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import javax.persistence.EntityNotFoundException;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Rolle wurde nicht in der Datenbank gefunden!")
+public class RoleNotFoundException extends EntityNotFoundException {
+}
