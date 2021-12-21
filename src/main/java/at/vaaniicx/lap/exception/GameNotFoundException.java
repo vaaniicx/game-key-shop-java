4153083fdf43f5d8082a5331356bae60d66ad3fe
Vanessa <vanessakausl@gmail.com>
2021-12-21 09:35:51 +0100

    Added custom exceptions

---
+++ src/main/java/at/vaaniicx/lap/exception/GameNotFoundException.java
@@ -0,0 +1,10 @@
+package at.vaaniicx.lap.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import javax.persistence.EntityNotFoundException;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Spiel wurde nicht in der Datenbank gefunden!")
+public class GameNotFoundException extends EntityNotFoundException {
+}
---
+++ src/main/java/at/vaaniicx/lap/exception/GamePictureNotFoundException.java
@@ -0,0 +1,11 @@
+package at.vaaniicx.lap.exception;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import javax.persistence.EntityNotFoundException;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Spielbild wurde nicht in der Datenbank gefunden!")
+public class GamePictureNotFoundException extends EntityNotFoundException {
+}
